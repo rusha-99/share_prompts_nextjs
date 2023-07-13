c77992c59016198ec41426872b808b204a521437
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-07-13 22:26:29 +0530

    use useState

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,6 +8,10 @@ import {signIn, signOut, useSession, getProviders} from "next-auth/react"
 
 
 const Nav = () => {
+  const isUserLoggedIn = false;
+
+  const [providers, setProviders] = useState(null);
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -20,6 +24,28 @@ const Nav = () => {
         ></Image>
         <p className="logo_text">Promptopia</p>
       </Link>
+      {/* Desktop Navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">Create Post</Link>
+            <button type="button" onClick={signOut} className="outline_btn">Sign Out</button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt = "profile"
+              
+              ></Image>
+            </Link>
+          </div>
+        ):(
+          <>
+          </>
+        )}
+      </div>
     </nav>
   )
 }
