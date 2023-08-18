7e6465b2a0af63bc497407079b4a47da14dd7a51
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-08-18 23:27:20 +0530

    update

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,12 +1,9 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
+import { connectToDB } from "@utils/database";
 
+import User from "@models/user";
 
-
-console.log({
-  clientId: process.env.GOOGLE_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-})
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -14,8 +11,34 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
-  async signIn({ profile }) {},
+    async session({ session }) {
+        const sessionUser = await User.findOne({ email: session.user.email })
+        session.user.id = sessionUser._id.toString
+        return session 
+  },
+    async signIn({ profile }) {
+        try { 
+            await connectToDB();
+
+            //if a user already exists, return true
+            const userExists = await User.findOne({ email: profile.email })
+
+            //if not,create a new user
+            if (!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.picture
+                })
+
+            }
+            return true;
+        }
+        catch (error) {
+            console.log(error)
+            return false
+        }
+  },
 })
 
 export {handler as GET, handler as POST}
