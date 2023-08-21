4be9d837a2e496c65f79084f2422ad57f1d567d5
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-08-21 23:22:46 +0530

    update

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,33 +11,34 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+
+  callbacks: {
     async session({ session }) {
-        const sessionUser = await User.findOne({ email: session.user.email })
-        session.user.id = sessionUser._id.toString
-        return session 
-  },
+      const sessionUser = await User.findOne({ email: session.user.email })
+      session.user.id = sessionUser._id.toString
+      return session
+    },
     async signIn({ profile }) {
-        try { 
-            await connectToDB();
+      try {
+        await connectToDB()
 
-            //if a user already exists, return true
-            const userExists = await User.findOne({ email: profile.email })
+        //if a user already exists, return true
+        const userExists = await User.findOne({ email: profile.email })
 
-            //if not,create a new user
-            if (!userExists) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ", "").toLowerCase(),
-                    image: profile.picture
-                })
-
-            }
-            return true;
+        //if not,create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(' ', '').toLowerCase(),
+            image: profile.picture,
+          })
         }
-        catch (error) {
-            console.log(error)
-            return false
-        }
+        return true
+      } catch (error) {
+        console.log(error)
+        return false
+      }
+    },
   },
 })
 
