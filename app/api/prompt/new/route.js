5579a9435a4dc0e1f2e03579e614a32623a9f042
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-08-24 09:55:00 +0530

    Update route.js

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,21 +1,14 @@
-import { connectToDB } from "@utils/database";
-import Prompt from "@models/prompt";
+import Prompt from '@models/prompt'
+import { connectToDB } from '@utils/database'
 
-export const POST = async (req) => {
-    const { userId, prompt, tag } = await req.json();
+export const GET = async (request) => {
+  try {
+    await connectToDB()
 
-    try {
+    const prompts = await Prompt.find({}).populate('creator')
 
-        await connectToDB()
-        const newPrompt = new Prompt({
-            creator: userId,
-            prompt,
-            tag
-        })
-        await newPrompt.save();
-        return new Response(JSON.stringify(newPrompt), {status: 201})
-        
-    } catch (error) {
-        return new Response("Failed to create a new prompt", {status: 500})
-    }
+    return new Response(JSON.stringify(prompts), { status: 200 })
+  } catch (error) {
+    return new Response('Failed to fetch all prompts', { status: 500 })
+  }
 }
