f4a1c876e9c9788fc80878bbf33ca8af11cff423
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-08-30 21:58:04 +0530

    load user image in prompt card

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,9 +6,22 @@ import { useSession } from 'next-auth/react'
 import { usePathname, useRouter } from 'next/navigation'
 
 
-const PromptCard = () => {
+const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
   return (
-    <div>PromptCard</div>
+    <div className='prompt_card'>
+      <div className="flex justify-between items-start gap-5">
+        <div>
+          <Image
+            src={post.creator.image}
+            alt="user image"
+            width={40}
+            height={40}
+            className="rounded-full object-contain"
+          
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
