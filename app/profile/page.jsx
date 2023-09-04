180fbc8b3408a123ae6b5a0cc2670085c5b1768b
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-09-04 23:31:30 +0530

    Update page.jsx

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,10 +6,21 @@ import { useState, useEffect } from 'react'
 import { useSession } from 'next-auth/react'
 import Profile from "@components/profile"
 
-const Profile = () => {
+const MyProfile = () => {
+    const handleEdit = () => {
+        
+    }
+
+    const handleDelete = async() => {    
+    }
   return (
-    <div>Profile</div>
+      <Profile
+          name="My"
+          desc="Welcome to your personalized profile page"
+          data={[]}
+          handleEdit={handleEdit}
+    handleDelete= {handleDelete}  />
   )
 }
 
-export default Profile
+export default MyProfile
