3545be92ff9c2e8b178ef1ecfadd2cbe98393ade
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-09-04 23:19:22 +0530

    Create page.jsx

---
+++ app/profile/page.jsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from 'react'
+
+import { useState, useEffect } from 'react'
+import { useSession } from 'next-auth/react'
+import Profile from "@components/profile"
+
+const Profile = () => {
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default Profile
